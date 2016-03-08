4d2ca5210f88a016c462234e193cc20843552ab2
jzaicn <jzaicn@163.com>
2016-03-08 10:47:21 +0800

    temp

--- Scripts/Model/FallItemModel.js
+++ Scripts/Model/FallItemModel.js
@@ -11,37 +11,28 @@ var FallItemModel = qc.CatchGame.FallItemModel = function() {
     self._y = 0;
     self._type = "";
     self._speed = 0;
-
-
-
-
-
-
-
-
-    self._icon = "";
-    self.id = "";
-    self._x = 0;
-    self._y = 0;
-    self.noRepeatRadius = 0;
-    self.noOtherRadius = 0;
-    self.score = 0;
-    self.o = null;
+    self._size = 0;
+    self._collision = 0;
 };
 FallItemModel.prototype = {};
 FallItemModel.prototype.constructor = FallItemModel;
 
 //更新自己坐标同时，同步到外部元素
 Object.defineProperties(FallItemModel.prototype, {
+    icon: {
+        get: function(){
+            return this._icon;
+        },
+        set: function(v){
+            this._icon = v;
+        }
+    },
     x: {
         get: function() {
             return this._x;
         },
         set: function(v) {
             this._x = v;
-            if (this.o) {
-                this.o.x = v;
-            };
         }
     },
     y: {
@@ -50,72 +41,58 @@ Object.defineProperties(FallItemModel.prototype, {
         },
         set: function(v){
             this._y = v;
-            if (this.o) {
-                this.o.y = v;
-            };
         }
-    }
+    },
+    type: {
+        get: function(){
+            return this._type;
+        },
+        set: function(v){
+            this._type = v;
+        }
+    },
+    speed: {
+        get: function(){
+            return this._speed;
+        },
+        set: function(v){
+            this._speed = v;
+        }
+    },
+    size: {
+        get: function(){
+            return this._size;
+        },
+        set: function(v){
+            this._size = v;
+        }
+    },
+    collision: {
+        get: function(){
+            return this._collision;
+        },
+        set: function(v){
+            this._collision = v;
+        }
+    },
 });
-
-//初始化
-FallItemModel.prototype.init = function(id,_icon,x,y,noRepeatRadius,noOtherRadius,score) {
-    var self = this;
-
-    self._icon = _icon;
-    self.id = id;
-    self.x = x;
-    self.y = y;
-    self.noRepeatRadius = noRepeatRadius;
-    self.noOtherRadius = noOtherRadius;
-    self.score = score;
-};
-
-//初始化
-FallItemModel.prototype.initObj = function(Obj) {
-    var self = this;
-
-    self._icon = Obj._icon;
-    self.id = Obj.id;
-    self.noRepeatRadius = Obj.noRepeatRadius;
-    self.noOtherRadius = Obj.noOtherRadius;
-    self.score = Obj.score;
-};
 
 //克隆自身
 FallItemModel.prototype.clone = function() {
     var self = this;
     var obj = new FallItemModel();
 
-    obj._icon = self._icon;
-    obj.id = self.id;
-    obj.x = self.x;
-    obj.y = self.y;
-    obj.noRepeatRadius = self.noRepeatRadius;
-    obj.noOtherRadius = self.noOtherRadius;
-    obj.score = self.score;
+    self._icon =objf._icon ;
+    self._x =obj._x;
+    self._y =obj._y;
+    self._type =objf._type ;
+    self._speed =obj._speed;
+    self._size =obj._size;
+    self._collision =obj._collision;
 
     return obj;
 };
 
-//对象用于分派的信息
-FallItemModel.prototype.getInfo = function(status) {
-    var self = this;//TODO: 被销毁后返回的内容，用于设置到对话框
-    var info = {
-        eventType : 1,
-        score : self.score,
-    };
-    return info;
-};
-
-
-//如果是某种表情则替换
-FallItemModel.prototype.onChangeEvent = function() {
-    var self = this;
-    if (self._icon == "7.png" || self._icon == "8.png") {
-        self.o.frame = "9.png";
-        self.timer = setTimeout(function(){
-            clearTimeout(self.timer);
-            self.o.frame = self._icon;
-        }, 1000);
-    };
+FallItemModel.prototype.loadSetting = function() {
+    // 从数据源加载构造参数
 };
